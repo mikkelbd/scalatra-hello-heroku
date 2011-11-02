de3a97641513b11bd04e739edcb11de9f8c74e60
Mikkel Byrsø Dan-Rognlie <mikkel@decorus.dk>
2011-11-02 13:09:48 +0100

    Test change for heroku push

--- src/main/scala/MyScalatraFilter.scala
+++ src/main/scala/MyScalatraFilter.scala
@@ -7,7 +7,7 @@ class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
   get("/") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
+        <h1>Hello, Scala!</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
       </body>
     </html>
