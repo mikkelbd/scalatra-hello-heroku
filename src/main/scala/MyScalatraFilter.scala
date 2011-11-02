e9992c7d3e0f047f43ac37b30b2e6b1295a759e0
Mikkel Byrsø Dan-Rognlie <mikkel@decorus.dk>
2011-11-02 19:01:23 +0100

    small change

--- src/main/scala/MyScalatraFilter.scala
+++ src/main/scala/MyScalatraFilter.scala
@@ -7,8 +7,8 @@ class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
   get("/") {
     <html>
       <body>
-        <h1>Hello, Scala!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <h1>Hello, javaBin!</h1>
+        <!--Say <a href="hello-scalate">hello to Scalate</a>.-->
       </body>
     </html>
   }
